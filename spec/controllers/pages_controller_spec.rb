e6db3f065363b3a1cdb4c6b8d00b7f81e1899bbe
Karen Kim <karenkimx88@yahoo.com>
2011-03-20 04:51:35 -0400

    title changed

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views  
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -16,4 +17,16 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+      it "should be successful" do
+          get 'about'
+          response.should be_success
+      end
+
+      it "should have the right title" do
+        get 'about'
+        response.should have_selector("title",
+                                    :content => "Ruby on Rails Tutorial Sample App | About")
+      end
+    end
 end
